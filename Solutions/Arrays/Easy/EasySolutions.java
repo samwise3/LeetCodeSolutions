73494e3933736269d5fdb53bbb2184cb02d9305a
samwise3 <ger7mm@virginia.edu>
2024-08-14 08:27:27 -0700

    lengthOfLastWord

--- Solutions/Arrays/Easy/EasySolutions.java
+++ Solutions/Arrays/Easy/EasySolutions.java
@@ -43,5 +43,15 @@ Consider the number of unique elements of nums to be k, to get accepted, you nee
         return j;
     }
 
+    /*
+    Given a string s consisting of words and spaces, return the length of the last word in the string.
+
+    A word is a maximal substring consisting of non-space characters only.
+     */
+    public int lengthOfLastWord(String s) {
+        String[] stringArray = s.split(" ");
+        int max = stringArray.length-1;
+        return stringArray[max].length();
+    }
     
 }
